4284821e5f62868f3efd1076165470ba441942f9
247017 Jeremi Cichawa <242211@edu.p.lodz.pl>
2024-10-08 13:27:25 +0200

    Added functions with lookup tables. Added negative.

--- IP.py
+++ IP.py
@@ -16,6 +16,7 @@ def saveImage(image_matrix, output_path):
     new_image.save(output_path)
     print(f"Image saved at {output_path}")
 
+
 def brightnessChangerFlat(image_matrix, brightness_change, sign_negative):
     output_matrix = np.zeros_like(image_matrix, dtype=np.uint8)
     if len(image_matrix.shape) == 2:  
@@ -82,9 +83,34 @@ def brightnessChangerGamma(image_matrix, brightness_change):
     return output_matrix
 
 
+def brightnessChangerGammaLT(image_matrix, brightness_change):
+    if(brightness_change < 0):
+        print("Wrong brightness_changer!")
+        return image_matrix
+    output_matrix = np.zeros_like(image_matrix, dtype=np.uint8)
+    lookup_table = {i: 0 for i in range(256)}
+    for i in range(1,256):
+        if (255/i < brightness_change):
+            lookup_table[i] = 255
+        else:
+            lookup_table[i] = i*brightness_change  
+    if len(image_matrix.shape) == 2:  
+        # Grayscale image
+        height, width = image_matrix.shape
+        for i in range(height):
+            for j in range(width):
+                output_matrix[i,j] = lookup_table[image_matrix[i,j]]
+    else:
+        # RGB Image
+        height, width, channels = image_matrix.shape
+        for i in range(height):
+            for j in range(width):
+                for k in range(channels):
+                   output_matrix[i,j,k] = lookup_table[image_matrix[i,j,k]] 
+    return output_matrix
+
 def contrastChanger(image_matrix, contrast_factor):
     output_matrix = np.zeros_like(image_matrix, dtype=np.uint8)
-    
     if len(image_matrix.shape) == 2:  # Grayscale image
         height, width = image_matrix.shape
         for i in range(height):
@@ -130,6 +156,48 @@ def contrastChanger(image_matrix, contrast_factor):
                     
     return output_matrix
 
+def contrastChangerLT(image_matrix, contrast_factor):
+    output_matrix = np.zeros_like(image_matrix, dtype=np.uint8)
+    lookup_table = {int(i): 0 for i in range(256)}
+    for i in range(256):
+        if(contrast_factor*(i-128)+128 > 255):
+            lookup_table[i] = 255
+        elif(contrast_factor * (i - 128) + 128 < 0):
+            lookup_table[i] = 0
+        else:
+            lookup_table[i] = contrast_factor*(i-128) + 128
+    if len(image_matrix.shape) == 2:  # Grayscale image
+        height, width = image_matrix.shape
+        for i in range(height):
+            for j in range(width):
+                output_matrix[i,j] = lookup_table[image_matrix[i,j]]
+    else:
+        # RGB Image
+        height, width, channels = image_matrix.shape
+        for i in range(height):
+            for j in range(width):
+                for k in range(channels):
+                    output_matrix[i,j,k] = lookup_table[image_matrix[i,j,k]]
+    return output_matrix
+
+def negative(image_matrix):
+    output_matrix = np.zeros_like(image_matrix, dtype=np.uint8)
+    lookup_table = {int(i): 0 for i in range(256)}
+    for i in range(256):
+        lookup_table[i] = 255 - i
+    if len(image_matrix.shape) == 2:  # Grayscale image
+        height, width = image_matrix.shape
+        for i in range(height):
+            for j in range(width):
+                output_matrix[i,j] = lookup_table[image_matrix[i,j]]
+    else:
+        # RGB Image
+        height, width, channels = image_matrix.shape
+        for i in range(height):
+            for j in range(width):
+                for k in range(channels):
+                    output_matrix[i,j,k] = lookup_table[image_matrix[i,j,k]]
+    return output_matrix
 
 ### CMD commands
 def main():
@@ -139,6 +207,7 @@ def main():
 
     command = sys.argv[1]
     image_path = sys.argv[2]
+    output_path = sys.argv[len(sys.argv)-1]
 
     # Load the image
     try:
@@ -155,7 +224,6 @@ def main():
         try:
             brightness_value = int(sys.argv[3])
             sign_negative = brightness_value < 0  # Determine if brightness is increasing or decreasing
-            output_path = sys.argv[4]
             modified_matrix = brightnessChangerFlat(matrix, abs(brightness_value), sign_negative)
             saveImage(modified_matrix, output_path)
         except ValueError:
@@ -168,8 +236,19 @@ def main():
             sys.exit(1)
         try:
             brightness_factor = float(sys.argv[3])
-            output_path = sys.argv[4]
             modified_matrix = brightnessChangerGamma(matrix, brightness_factor)
+            saveImage(modified_matrix, output_path)
+        except ValueError:
+            print("Error: Brightness factor must be a valid float.")
+            sys.exit(1)
+
+    elif command == '--brightnessGammaLT':
+        if len(sys.argv) != 5:
+            print("Usage: python script.py --brightnessGamma <image_path> <brightness_factor> <output_path>")
+            sys.exit(1)
+        try:
+            brightness_factor = float(sys.argv[3])
+            modified_matrix = brightnessChangerGammaLT(matrix, brightness_factor)
             saveImage(modified_matrix, output_path)
         except ValueError:
             print("Error: Brightness factor must be a valid float.")
@@ -181,8 +260,30 @@ def main():
             sys.exit(1)
         try:
             contrast_factor = float(sys.argv[3])
-            output_path = sys.argv[4]
             modified_matrix = contrastChanger(matrix, contrast_factor)
+            saveImage(modified_matrix, output_path)
+        except ValueError:
+            print("Error: Contrast factor must be a valid float.")
+            sys.exit(1)
+        
+    elif command == '--contrastLT':
+        if len(sys.argv) != 5:
+            print("Usage: python script.py --contrast <image_path> <contrast_factor> <output_path>")
+            sys.exit(1)
+        try:
+            contrast_factor = float(sys.argv[3])
+            modified_matrix = contrastChangerLT(matrix, contrast_factor)
+            saveImage(modified_matrix, output_path)
+        except ValueError:
+            print("Error: Contrast factor must be a valid float.")
+            sys.exit(1)
+
+    elif command == '--negative':
+        if len(sys.argv) != 4:
+            print("Usage: python script.py --negative <image_path> <output_path>")
+            sys.exit(1)
+        try:
+            modified_matrix = negative(matrix)
             saveImage(modified_matrix, output_path)
         except ValueError:
             print("Error: Contrast factor must be a valid float.")
